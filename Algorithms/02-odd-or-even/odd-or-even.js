1602a2d98533f8eab208f52d491429bdf509a1c0
Bahia369 <hsbcoding@gmail.com>
2021-05-21 13:51:49 +0100

    updated
    code updated

--- Algorithms/02-odd-or-even/odd-or-even.js
+++ Algorithms/02-odd-or-even/odd-or-even.js
@@ -1,13 +1,7 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-let oddOrEven = function(num) {
-    let num = /2;
-    return "even"
-}
-else {
-    return "false";
-};
+
 // If `num`
 // is evenly divisible by 2,
 // return the string "even".
